295c4f9d7bc383c92af2118d877eec0f77caf1d8
Lakhan5556 <reddylnr9@gmail.com>
2024-09-04 22:10:09 -0400

    commiting java-angular-status repo

---
+++ README.md
@@ -0,0 +1,82 @@
+# :zap: Spring Java Angular Status
+
+* Angular client with Java Spring boot back-end used to display hardware status
+* **Note:** to open web links in a new window use: _ctrl+click on link_
+
+![GitHub repo size](https://img.shields.io/github/repo-size/AndrewJBateman/java-angular-status?style=plastic)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/AndrewJBateman/java-angular-status?style=plastic)
+![GitHub Repo stars](https://img.shields.io/github/stars/AndrewJBateman/java-angular-status?style=plastic)
+![GitHub last commit](https://img.shields.io/github/last-commit/AndrewJBateman/java-angular-status?style=plastic)
+
+## :page_facing_up: Table of contents
+
+* [Java Angular Status](#java-angular-status)
+    * [:page_facing_up: Table of contents](#page_facing_up-table-of-contents)
+    * [:books: General info](#books-general-info)
+    * [:camera: Screenshots](#camera-screenshots)
+    * [:signal_strength: Technologies](#signal_strength-technologies)
+    * [:floppy_disk: Setup](#floppy_disk-setup)
+    * [:computer: Code Examples](#computer-code-examples)
+    * [:cool: Features](#cool-features)
+    * [:clipboard: Status & To-Do List](#clipboard-status--to-do-list)
+    * [:clap: Inspiration](#clap-inspiration)
+    * [:file_folder: License](#file_folder-license)
+    * [:envelope: Contact](#envelope-contact)
+
+## :books: General info
+
+* CTRL + ALT + L formats everything
+* [Java Beans](https://en.wikipedia.org/wiki/JavaBeans) standardized objects used
+
+## :signal_strength: Technologies
+
+**Backend**
+* [Spring project initialiser](https://start.spring.io/) & [Spring Java Tools v4](https://spring.io/tools)
+* [Project Lombok](https://www.baeldung.com/intro-to-project-lombok) to autogenerate Java bytecode
+* [Java Persistence API - JPA](https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.repositories), the standard way of persisting Java objects into relational databases. The JPA consists of two parts: a mapping subsystem to map classes onto relational tables as well as an EntityManager API to access the objects, define and execute queries, and more. Includes standard fins and savew methods like `findAll()`, `findAllById(Iterable<ID> var1)`, `saveAll(Iterable<S> var1)` etc.
+* [MySQL Structured Query Language Database v8](https://www.mysql.com/)
+
+**Frontend**
+* [Angular v13](https://angular.io/) frontend framework
+* [rxjs v7](https://rxjs.dev/) reactive extensions library
+
+## :camera: Screenshots
+
+![Example screenshot](./img/java-angular.png)
+
+## :floppy_disk: Setup
+
+* Backend: tba
+* Frontend: change directory to `/modules/frontend`, run `npm i` then run `ng serve`
+
+## :computer: Code Examples
+
+* tba
+
+```java
+
+```
+
+## :cool: Features
+
+* tba
+
+## :clipboard: Status & To-Do List
+
+* Status: In work. Backend sends data to MySQL database. Frontend added.
+* Status: In work. Backend sends data to MySQL database
+* To-Do: Complete backend and frontend and test
+
+## :clap: Inspiration
+
+* [Amigoscode: Spring Boot and Angular Full Stack Development | 4 Hour Course](https://www.youtube.com/watch?v=8ZPsZBcue50&t=3662s)
+* [Generating DB Schema in 10 seconds with MySQL Workbench](https://www.youtube.com/watch?v=RbKEYDtkAJI)
+* [URTechDotCa: Download, Install & Configure MySQL Older Versions (like 5.5) on Windows 10 or Server 2016](https://www.youtube.com/watch?v=bnsBAcSPeGg)
+
+## :file_folder: License
+
+* N/A
+
+## :envelope: Contact
+
+* Repo created by [ABateman](https://github.com/AndrewJBateman), email: gomezbateman@yahoo.com
---
+++ serverapp/src/app/app.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Server } from './interface/server';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, tap, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'serverapp';
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.filterSubject.asObservable();
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        this.dataSubject.next(response);
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress).pipe(
+      map((response) => {
+        const index = this.dataSubject.value.data.servers.findIndex(
+          (server) => server.id === response.data.server.id
+        );
+        this.dataSubject.value.data.servers[index] = response.data.server;
+        this.filterSubject.next('');
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
+    this.appState$ = this.serverService.save$(serverForm.value as Server).pipe(
+      map((response) => {
+        this.dataSubject.next({
+          ...response,
+          data: {
+            servers: [
+              response.data.server,
+              ...this.dataSubject.value.data.servers,
+            ],
+          },
+        });
+        document.getElementById('closeModal').click();
+        this.isLoading.next(false);
+        serverForm.resetForm({ status: this.Status.SERVER_DOWN });
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.isLoading.next(false);
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService
+      .filter$(status, this.dataSubject.value)
+      .pipe(
+        map((response) => {
+          return {
+            dataState: DataState.LOADED_STATE,
+            appData: response,
+          };
+        }),
+        startWith({
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+}
